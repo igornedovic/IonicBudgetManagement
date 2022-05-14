74f33fe839886ce4078c620c67cff286c8afccb7
Igor <igornedovic@gmail.com>
2022-05-14 18:16:55 +0200

    Registration page designed

---
+++ src/app/auth/register/register.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+  registerForm: FormGroup;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.registerForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  onRegister() {
+    console.log(this.registerForm.value);
+  }
+}
